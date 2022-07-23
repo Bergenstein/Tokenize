7016ce8bd3cbcd03b67bc859d6819c7d3951875e
Israel Bergenstein <55375606+Bergenstein@users.noreply.github.com>
2022-07-23 21:26:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Tokenize/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> tokenise(std::string csvLine, char separator)
+{
+    std::vector<std::string>tokens;
+    signed int start, end;  
+    start = csvLine.find_first_not_of(separator, 0); //first position after 0
+    std::string token;
+    do
+    {
+        end = csvLine.find_first_of(separator, start); //first separator after start
+        if(start == csvLine.length() || start == end) break;
+
+        if(end >=0)
+        {
+            token = csvLine.substr(start, end-start);
+        }
+
+        else token = csvLine.substr(start, csvLine.length()-start);
+
+        tokens.push_back(token);
+        start = end+1;
+    }
+
+    while(end != std::string::npos);
+
+    return tokens;
+}
+
+int main(void)
+{
+    return 0;
+}
